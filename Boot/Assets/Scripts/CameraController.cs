9392b6317f2cc425c7394d1a8a3f02722973b2e4
JENO <uberjoel25@yahoo.com>
2017-01-12 22:30:58 -0500

    Added Touch Controls
    modified movement so players won't get stuck on invisible barriers

--- Boot/Assets/Scripts/CameraController.cs
+++ Boot/Assets/Scripts/CameraController.cs
@@ -5,24 +5,48 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     public PlayerController player;
+
+
     float targetRatio = 16f / 9f;
+    float windowRatio = (float)Screen.width / (float)Screen.height;
 
     private Vector3 lastPlayerPosition;
     private float distanceToMove;
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
+        float scaleHeight = windowRatio / targetRatio;
+        Camera myCam = GetComponent<Camera>();
+        if (scaleHeight < 1.0f)
+        {
+            Rect rect = myCam.rect;
+            rect.width = 1.0f;
+            rect.height = scaleHeight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleHeight) / 2.0f;
+            myCam.rect = rect;
+        }
+        else
+        {
+            float scaleWidth = 1.0f / scaleHeight;
+            Rect rect = myCam.rect;
+            rect.width = scaleWidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scaleWidth) / 2.0f;
+            rect.y = 0;
+            myCam.rect = rect;
+        }
+
         player = FindObjectOfType<PlayerController>();
         lastPlayerPosition = player.transform.position;
-        Camera myCam = GetComponent<Camera>();
-        myCam.aspect = targetRatio;
+
     }
 
     // Update is called once per frame
-    void Update ()
+    void Update()
     {
         distanceToMove = player.transform.position.x - lastPlayerPosition.x;
         transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);
-        lastPlayerPosition = player.transform.position;	
-	}
+        lastPlayerPosition = player.transform.position;
+    }
 }
--- Boot/Assets/Scripts/PlayerController.cs
+++ Boot/Assets/Scripts/PlayerController.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+
+    //player movement variables
     public float movespeed = 2;
     public float jumpForce = 8;
-
     public bool grounded;
     public LayerMask whatIsGround;
+
+    //Touch Screen variables
+    private float maxTime = 0.5f;
+    private float minSwipeDistance = 25;
+    float swipeDistance;
+    float swipeTime;
+    float startTime;
+    float endTime;
+    Vector3 startPos;
+    Vector3 endPos;
+
 
     private Rigidbody2D playerRB;
     private Collider2D playerCol;
@@ -25,10 +37,52 @@ public class PlayerController : MonoBehaviour {
         grounded = Physics2D.IsTouchingLayers(playerCol, whatIsGround);
         playerRB.velocity = new Vector2(movespeed, playerRB.velocity.y);
 
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                startTime = Time.time;
+                startPos = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                endTime = Time.time;
+                endPos = touch.position;
+                swipeDistance = (endPos - startPos).magnitude;
+                swipeTime = (endTime - startTime);
+
+                if (swipeTime < maxTime && swipeDistance > minSwipeDistance)
+                {
+                    Swipe();
+                }
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                /*
+                endTime = Time.time;
+                endPos = touch.position;
+
+                swipeDistance = (endPos - startPos).magnitude;
+                swipeTime = (endTime - startTime);
+
+                if (swipeTime < maxTime && swipeDistance > minSwipeDistance)
+                {
+                    Swipe();
+                }
+                */
+                touch = new Touch();
+            }
+        }
+
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             Jump();
-                
+
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            DownActions();
         }
 	}
 
@@ -37,6 +91,50 @@ public class PlayerController : MonoBehaviour {
         if (grounded)
         {
             playerRB.velocity = new Vector2(playerRB.velocity.x, jumpForce);
+        }
+    }
+
+    void DownActions()
+    {
+        if (grounded)
+        {
+            //playerRB.velocity = new Vector2(playerRB.velocity.x, jumpForce);
+        }
+        else
+        {
+            playerRB.velocity = new Vector2(playerRB.velocity.x, -jumpForce * 2);
+        }
+    }
+
+    void Swipe()
+    {
+        Vector2 distance = endPos - startPos;
+        if (Mathf.Abs(distance.x) > Mathf.Abs(distance.y))
+        {
+            //horizontal swipe
+            if (distance.x > 0)
+            {
+                //Right swipe
+            }
+            else if (distance.x < 0)
+            {
+                //LeftSwipe
+            }
+        }
+
+        else if (Mathf.Abs(distance.x) < Mathf.Abs(distance.y))
+        {
+            //vertical swipe
+            if (distance.y > 0)
+            {
+                //Up swipe
+                Jump();
+            }
+            else if (distance.y < 0)
+            {
+                //DownSwipe
+                DownActions();
+            }
         }
     }
 }
